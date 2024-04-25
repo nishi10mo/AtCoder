68bbeaa06be954c1912a311f2ad52daf84357ec7
nishi10mo <onionolivetomato@gmail.com>
2024-04-25 11:21:29 +0900

    ABC098 C 2nd

--- practice/AtCoder Beginner Contest/ABC098/C.py
+++ practice/AtCoder Beginner Contest/ABC098/C.py
@@ -1,17 +1,29 @@
+# def Attention(n, s):
+#   pre = s.count("E")
+#   cur = pre
+#   for i in range(n):
+#     if pre < cur:
+#       cur = pre
+#     if s[i]=="W":
+#       pre += 1
+#     if s[i]=="E":
+#       pre -= 1
+#   if pre <= cur:
+#     return pre
+#   else:
+#     return cur
+
 def Attention(n, s):
-  pre = s.count("E")
-  cur = pre
+  tmp = s.count("E")
+  ans = tmp
   for i in range(n):
-    if pre < cur:
-      cur = pre
     if s[i]=="W":
-      pre += 1
+      tmp += 1
     if s[i]=="E":
-      pre -= 1
-  if pre <= cur:
-    return pre
-  else:
-    return cur
+      tmp -= 1
+    if tmp < ans:
+      ans = tmp
+  return ans
 
 n = int(input())
 s = list(input())
