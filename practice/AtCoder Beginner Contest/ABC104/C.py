119b8805f2f0dbcc395b586ed75619318ba64306
nishi10mo <onionolivetomato@gmail.com>
2024-04-26 21:54:38 +0900

    ABC104 C

--- practice/AtCoder Beginner Contest/ABC104/C.py
+++ practice/AtCoder Beginner Contest/ABC104/C.py
@@ -1,16 +1,13 @@
 def AllGreen(d, g, info):
     if d == 0:
-        return 0
-    n = min(g//(100*d), info[d][0])
-    score = 0
-    for i in range(d-1, -1, -1):
-        for j in range(info[i][1]):
-            score += 100*(i+1)
-            c += 1
-            if j == info[i][1]-1:
-                score += info[i][2]
-            if score >= g:
-                return c
+        return 1e9
+    n = min(g//(100*d), info[d-1][0])
+    s = 100*d*n
+    if n==info[d-1][0]:
+        s += info[d-1][1]
+    if s < g:
+        n += AllGreen(d-1, g-s, info)
+    return min(n, AllGreen(d-1, g, info))
 
 d, g = map(int, list(input().split()))
 info = []
