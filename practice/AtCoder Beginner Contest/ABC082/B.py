4655ba427bedfc641885d2db5485885aec977f8f
nishi10mo <onionolivetomato@gmail.com>
2024-04-27 23:04:35 +0900

    ABC082 B

--- practice/AtCoder Beginner Contest/ABC082/B.py
+++ practice/AtCoder Beginner Contest/ABC082/B.py
@@ -1,1 +1,11 @@
-def TwoAnagrams():
+def TwoAnagrams(s, t):
+  s = sorted(s)
+  t = sorted(t, reverse=True)
+  if s < t:
+    return "Yes"
+  else:
+    return "No"
+
+s = input()
+t = input()
+print(TwoAnagrams(s, t))
