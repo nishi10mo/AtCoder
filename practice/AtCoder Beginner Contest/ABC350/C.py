0f4cdf2cccad4aa95b0be8dbd1dd76f3d02835bb
nishi10mo <onionolivetomato@gmail.com>
2024-05-06 16:49:44 +0900

    ABC350 C

--- practice/AtCoder Beginner Contest/ABC350/C.py
+++ practice/AtCoder Beginner Contest/ABC350/C.py
@@ -1,15 +1,17 @@
 # Sort
-# 以下のコードはTLE
 def main():
     n = int(input())
-    A = list(map(int, list(input().split())))
+    A = [int(i)-1 for i in input().split()]
+    index = [0]*n  # 特定の数字がAのどこにあるかを記録
+    for i in range(n):
+        index[A[i]] = i
     c = 0  # 何回交換したかを数える
     rec = []  # 何番目と何番目を交換したかを記録
     for i in range(n):
-        if A[i] != i+1:
-            p = A.index(i+1, i+1, n)
-            rec.append([i+1, p+1])
-            A[i], A[p] = A[p], A[i]
+        if A[i] != i:
+            rec.append([i+1, index[i]+1])
+            A[i], A[index[i]] = A[index[i]], A[i]
+            index[A[index[i]]], index[i] = index[i], index[A[index[i]]]
             c += 1
     print(c)
     for i in range(c):
