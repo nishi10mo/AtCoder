2e5ee3f13c73a163162944f1ab67c7c63945917b
nishi10mo <onionolivetomato@gmail.com>
2024-05-14 13:03:26 +0900

    ABC353 B

--- practice/AtCoder Beginner Contest/ABC353/B.py
+++ practice/AtCoder Beginner Contest/ABC353/B.py
@@ -1,4 +1,22 @@
 # AtCoder Amusement Park
+def calc(N, K, A):
+  train = []
+  ans = 0
+  i = 0
+  while i < N:
+    train.append(A[i])
+    if sum(train) > K:
+      train = []
+      ans += 1
+      i -= 1
+    i += 1
+  ans += 1
+  return ans
+
 def main():
   N, K = [int(i) for i in input().split()]
   A = [int(i) for i in input().split()]
+  print(calc(N, K, A))
+
+if __name__=="__main__":
+  main()
