d20cbbaf6b8f5f9e90b2eb93092de85422b5c0d8
nishi10mo <onionolivetomato@gmail.com>
2024-06-10 22:43:09 +0900

    ABC341 C

---
+++ practice/AtCoder Beginner Contest/ABC341/C.py
@@ -0,0 +1,9 @@
+# Takahashi Gets Lost
+def main():
+    H, W, N = [int(i) for i in input().split()]
+    T = list(input())
+    S = []
+    for _ in range(H):
+        s = list(input())
+        S.append(s)
+    
