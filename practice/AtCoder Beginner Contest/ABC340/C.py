9b485b23a5d5c60e6849694d6291440e6b8a8848
nishi10mo <onionolivetomato@gmail.com>
2024-06-13 11:21:04 +0900

    ABC340 C

--- practice/AtCoder Beginner Contest/ABC340/C.py
+++ practice/AtCoder Beginner Contest/ABC340/C.py
@@ -1,29 +1,29 @@
 # Divide and Divide
-import math
+# import math
 
-def calc(board):
-    ans = 0
-    flag = False
-    for i in board:
-        if i >= 2:
-            board.remove(i)
-            x1 = math.floor(i/2)
-            x2 = math.ceil(i/2)
-            board.append(x1)
-            board.append(x2)
-            ans += i
-            flag = True
-    return board, ans, flag        
+# def calc(board):
+#     ans = 0
+#     flag = False
+#     for i in board:
+#         if i >= 2:
+#             board.remove(i)
+#             x1 = math.floor(i/2)
+#             x2 = math.ceil(i/2)
+#             board.append(x1)
+#             board.append(x2)
+#             ans += i
+#             flag = True
+#     return board, ans, flag    
+
+from functools import cache
+
+@cache
+def f(N):
+    return 0 if N==1 else f(N//2) + f((N+1)//2) + N
 
 def main():
     N = int(input())
-    board = [N]
-    flag = True
-    ans = 0
-    while flag == True:
-        board, ans_, flag = calc(board)
-        ans += ans_
-    print(ans)
+    print(f(N))
 
 if __name__=="__main__":
     main()
