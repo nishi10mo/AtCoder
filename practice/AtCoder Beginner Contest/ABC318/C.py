a0657f1114462233f17f8b2f7f4c468c2a6d5dcb
nishi10mo <onionolivetomato@gmail.com>
2024-07-08 14:17:51 +0900

    ABC318 C

--- practice/AtCoder Beginner Contest/ABC318/C.py
+++ practice/AtCoder Beginner Contest/ABC318/C.py
@@ -1,20 +1,33 @@
 # Blue Spring
+# 以下はTLE
+# def f(N, D, P, F):
+#   results = []
+#   F = sorted(F, reverse=True)
+#   flag = True
+#   while flag==True:
+#     if len(F) > D:
+#       Fl = F[:D]
+#     else:
+#       Fl = F
+#     if sum(Fl) > P:
+#       results += [P]
+#       F = F[D:]
+#     else:
+#       results += F
+#       flag = False
+#   return sum(results)
+
 def f(N, D, P, F):
-  results = []
-  F = sorted(F, reverse=True)
-  flag = True
-  while flag==True:
-    if len(F) > D:
-      Fl = F[:D]
-    else:
-      Fl = F
-    if sum(Fl) > P:
-      results += [P]
-      F = F[D:]
-    else:
-      results += F
-      flag = False
-  return sum(results)
+  F = sorted(F)
+  cumsum = [0]*N
+  cumsum[0] = F[0]
+  for i in range(1, N):
+    cumsum[i] = cumsum[i-1] + F[i]
+  k = (N + D - 1) // D
+  ans = P*k
+  for i in range(k):
+    ans = min(ans, cumsum[N-1-(i*D)]+(P*i))
+  return ans
 
 def main():
   N, D, P = [int(i) for i in input().split()]
