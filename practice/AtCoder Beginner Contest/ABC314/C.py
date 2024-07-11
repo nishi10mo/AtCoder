8467f7652fbea82b90583e52290422950ddc3748
nishi10mo <onionolivetomato@gmail.com>
2024-07-12 00:21:31 +0900

    ABC314 C

--- practice/AtCoder Beginner Contest/ABC314/C.py
+++ practice/AtCoder Beginner Contest/ABC314/C.py
@@ -1,4 +1,5 @@
 # Rotate Colored Subsequence
+import copy
 def f(N, M, S, C):
   count = [0]*M
   place = [0]*N
@@ -6,18 +7,28 @@ def f(N, M, S, C):
     count[v-1] += 1
     place[i] = count[v-1]
 
-  
-  align = [[] for _ in range(M)]
-  return align
+  align = []
+  for i in count:
+    tmp = [0]*i
+    align.append(tmp)
   for i in range(N):
     align[C[i]-1][place[i]-1] = S[i]
-  return align
+  for i in align:
+    i_ = copy.deepcopy(i)
+    i += i_*2
+  
+  result =[]
+  for i in range(N):
+    ans = align[C[i]-1][place[i]+count[C[i]-1]-2]
+    result.append(ans)
+  return result
 
 def main():
   N, M = [int(i) for i in input().split()]
   S = list(input())
   C = [int(i) for i in input().split()]
-  print(f(N, M, S, C))
+  result = f(N, M, S, C)
+  print("".join(result))
 
 if __name__=="__main__":
   main()
