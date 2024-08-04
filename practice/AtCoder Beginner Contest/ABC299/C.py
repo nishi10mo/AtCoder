e4ec71ec33f33d6cc9749b8f9a8e6d15259ef020
nishi10mo <onionolivetomato@gmail.com>
2024-08-04 18:52:58 +0900

    ABC299 C

--- practice/AtCoder Beginner Contest/ABC299/C.py
+++ practice/AtCoder Beginner Contest/ABC299/C.py
@@ -1,45 +1,25 @@
 # Dango
 def f(N, S):
-  i = 0
-  ans = -1
-  while i < N:
+  def calc(N, S):
+    ans = -1
     level = 0
-    if S[i]=="o":
-      level += 1
-      j = i+1
-    elif S[i]=="-" and S[i+1]=="o":
-      level += 1
-      j = i+2
-    else:
-      level = -1
-      j = i+1
-    while S[j]=="o":
-      level += 1
-      j += 1
-    i = j
-    ans = max(ans, level)
-
-def f(N, S):
-  i = 0
-  ans = -1
-  while i < N:
-    if S[i]=="o" and S[i+1]=="o":
-
-      level += 1
-      j = i+1
-    elif S[i]=="-" and S[i+1]=="o":
-      level += 1
-      j = i+2
-    else:
-      level = -1
-      j = i+1
-    while S[j]=="o":
-      level += 1
-      j += 1
-    i = j
-    ans = max(ans, level)
-
+    for i in range(N-1):
+      if S[i]=="o" and S[i+1]=="o":
+        level += 1
+      elif S[i]=="o" and S[i+1]=="-":
+        level += 1
+        ans = max(level, ans)
+        level = 0
+    return ans
+  ans1 = calc(N, S)
+  S = S[::-1]
+  ans2 = calc(N, S)
+  return max(ans1, ans2)
 
 def main():
   N = int(input())
   S = list(input())
+  print(f(N, S))
+
+if __name__=="__main__":
+  main()
