193ffeab42d037e27a775f145a776a990c711ae8
nishi10mo <onionolivetomato@gmail.com>
2024-09-25 23:09:56 +0900

    ABC274 C

--- practice/AtCoder Beginner Contest/ABC274/C.py
+++ practice/AtCoder Beginner Contest/ABC274/C.py
@@ -4,14 +4,19 @@ def f(N, A):
     for i in range(N):
         graph[2*(i+1)] = A[i]
         graph[2*(i+1)+1] = A[i]
-    results = []
-    for i in range(2*N+1):
-        
+    results = [0]
+    for i in range(1, 2*N+1):
+        parents = graph[i+1]
+        c = results[parents-1] + 1
+        results.append(c)
+    return results
 
 def main():
     N = int(input())
     A = [int(i) for i in input().split()]
-    print(f(N, A))
+    results = f(N, A)
+    for ans in results:
+        print(ans)
 
 if __name__=="__main__":
     main()
