b84d70d68cd1eb9593b9c8123449d7e5f57d75e0
nishi10mo <onionolivetomato@gmail.com>
2025-02-06 16:49:48 +0900

    ABC269 C

--- practice/AtCoder Beginner Contest/ABC269/C.py
+++ practice/AtCoder Beginner Contest/ABC269/C.py
@@ -1,3 +1,18 @@
 # Submask
+def f(N):
+    ans = []
+    i = N
+    while True:
+        ans.append(i)
+        if i == 0:
+            break
+        i = (i-1) & N
+    return ans[::-1]
+
 def main():
-  
+    N = int(input())
+    for ans in f(N):
+        print(ans)
+
+if __name__ == '__main__':
+    main()
