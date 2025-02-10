49be96f51a5fa2f33e5fd8b3c26259bd8c8c71b1
nishi10mo <onionolivetomato@gmail.com>
2025-02-10 23:53:11 +0900

    ABC276 C

--- practice/AtCoder Beginner Contest/ABC276/C.py
+++ practice/AtCoder Beginner Contest/ABC276/C.py
@@ -1,11 +1,18 @@
 # Previous Permutation
 from itertools import permutations
 def f(N, P):
-    
+    i = N - 2
+    while P[i] < P[i+1]:
+        i -= 1
+    j = N - 1
+    while P[j] > P[i]:
+        j -= 1
+    P[i], P[j] = P[j], P[i]
+    return P[:i+1] + P[:i:-1]
 
 def main():
     N = int(input())
-    P = tuple([int(i) for i in input().split()])
+    P = [int(i) for i in input().split()]
     print(*f(N, P))
 
 if __name__=="__main__":
