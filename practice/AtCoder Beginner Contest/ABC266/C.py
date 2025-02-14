ea8fac724d1f60ff17009fac4465e0b5d68adeb1
nishi10mo <onionolivetomato@gmail.com>
2025-02-14 19:17:37 +0900

    ABC266 C

--- practice/AtCoder Beginner Contest/ABC266/C.py
+++ practice/AtCoder Beginner Contest/ABC266/C.py
@@ -1,31 +1,22 @@
 # Convex Quadrilateral
-import numpy as np
-import math
 def f(A, B, C, D):
-    vector = [[] for _ in range(4)]
-    vector[0].append(B - A)
-    vector[0].append(D - A)
-    vector[1].append(A - B)
-    vector[1].append(D - A)
-    vector[2].append(B - C)
-    vector[2].append(D - C)
-    vector[3].append(C - D)
-    vector[3].append(A - D)
-    angles = []
-    for i in range(4):
-        inner_product = vector[i][0] @ vector[i][1]
-        product = np.linalg.norm(vector[i][0], ord=2) * np.linalg.norm(vector[i][1], ord=2)
-        degree = math.degrees(math.acos(inner_product / product))
-        angles.append(degree)
-    if round(sum(angles)) == 360:
+    def Check_CrossProduct(a, o, b):
+        cross_product = (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+        if cross_product > 0:
+            return True
+        else:
+            return False
+    
+    if Check_CrossProduct(B, A, D) and Check_CrossProduct(C, B, A) and Check_CrossProduct(D, C, B) and Check_CrossProduct(A, D, C):
         return "Yes"
-    return "No"
+    else:
+        return "No"
 
 def main():
-    A = np.array([int(i) for i in input().split()])
-    B = np.array([int(i) for i in input().split()])
-    C = np.array([int(i) for i in input().split()])
-    D = np.array([int(i) for i in input().split()])
+    A = [int(i) for i in input().split()]
+    B = [int(i) for i in input().split()]
+    C = [int(i) for i in input().split()]
+    D = [int(i) for i in input().split()]
     print(f(A, B, C, D))
 
 if __name__=="__main__":
